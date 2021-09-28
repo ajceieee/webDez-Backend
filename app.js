13a53680d4445383ba5dd04d40b55bba59b11347
shan-shaji <abrahamshanshaji@gmail.com>
2021-09-28 23:32:10 +0530

    feat: add health and root routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 
 const userRouter = require('./src/routes/user.router')
 const postRouter = require('./src/routes/post.router')
+const rootRouter = require('./src/routes/root.router')
+const healthRouter = require('./src/routes/health.router')
 
 const app = express()
 
@@ -12,7 +14,9 @@ if (process.env.NODE_ENV == 'development') {
 
 app.use(express.json())
 
+app.use('/health', healthRouter)
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/post', postRouter)
+app.use('/*', rootRouter)
 
 module.exports = app
